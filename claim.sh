db6525c630fe476d7307265e02600e2cb4496575
nn <muunala1022@gmail.com>
2024-10-29 12:13:32 +0800

    Update claim.sh

--- claim.sh
+++ claim.sh
@@ -4,7 +4,7 @@
 cd ~/ceremonyclient/node/
 
 # 执行第一个命令并提取账户地址
-output=$(./../client/qclient-2.0.1-linux-amd64 token balance)
+output=$(./../client/qclient-2.0.2.3-linux-amd64 token balance)
 echo "$output"
 
 # 提取地址
@@ -15,7 +15,7 @@ echo "提取的地址: $address"
 read -p "请输入下一个命令参数 (例如 0xxxx): " param1
 
 # 执行第二个命令
-output2=$(./../client/qclient-2.0.1-linux-amd64 cross-mint "$param1")
+output2=$(./../client/qclient-2.0.2.3-linux-amd64 cross-mint "$param1")
 echo "$output2"
 
 # 提取 peerPublicKey
@@ -26,7 +26,7 @@ echo "提取的 peerPublicKey: $peerPublicKey"
 read -p "请输入下一个字符串 (例如 0xxxx): " param2
 
 # 执行第三个命令
-output3=$(./../client/qclient-2.0.1-linux-amd64 cross-mint "$param2")
+output3=$(./../client/qclient-2.0.2.3-linux-amd64 cross-mint "$param2")
 echo "$output3"
 
 # 提取 peerPublicKey
